a739ad73b4f4d4551faa7d27d6b681e4f0fc48d8
Tim Clark <tim.clark@outlook.com>
2017-07-23 01:08:31 +0000

    asignment24

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
      resources :posts, except: [:index]
    end
    resources :users, only: [:new, :create]
+   post 'users/confirm' => 'users#confirm'
 
   get 'about' => 'welcome#about'
   
