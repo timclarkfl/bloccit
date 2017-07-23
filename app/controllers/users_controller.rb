9af24efa4041c0e1141551f0105e8a48ed6c8309
Tim Clark <tim.clark@outlook.com>
2017-07-23 01:23:33 +0000

    chkpoint25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,15 @@ class UsersController < ApplicationController
      @user = User.new
    end
    
+   def confirm
+
+     @user = User.new
+     @user.name = params[:user][:name]
+     @user.email = params[:user][:email]
+     @user.password = params[:user][:password]
+     @user.password_confirmation = params[:user][:password_confirmation]
+   end
+   
    def create
 
      @user = User.new
@@ -14,6 +23,7 @@ class UsersController < ApplicationController
 
      if @user.save
        flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+       create_session(@user)
        redirect_to root_path
      else
        flash.now[:alert] = "There was an error creating your account. Please try again."
