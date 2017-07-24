50972888b54b2c34537619572c1fed7cf2654b68
Tim Clark <tim.clark@outlook.com>
2017-07-24 02:27:43 +0000

    chkpoint26

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
  require 'random_data'
  
+  5.times do
+   User.create!(
+ # #3
+   name:     RandomData.random_name,
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+ users = User.all
+ 
   # Create Topics
  15.times do
    Topic.create!(
@@ -13,6 +23,7 @@
  50.times do
 
    Post.create!(
+     user:   users.sample,
      topic:  topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
@@ -29,8 +40,14 @@
    )
  end
  
+   user = User.first
+ user.update_attributes!(
+   email: 'tim.clark@outlook.com', # replace this with your personal email
+   password: 'helloworld'
+ )
  
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
