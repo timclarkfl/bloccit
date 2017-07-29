f4e33ad613dd6dd90783741318d95ace2ed10725
Tim Clark <tim.clark@outlook.com>
2017-07-29 19:07:17 +0000

    assignment30

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -13,4 +13,14 @@ class FavoriteMailer < ApplicationMailer
 
      mail(to: user.email, subject: "New comment on #{post.title}")
    end
+   
+   def new_post(post)
+       
+     headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
+     headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+     headers["References"] = "<post/#{post.id}@your-app-name.example>"
+     
+     @post = post
+      mail(to: user.email, subject: "You are following, #{post.title}")
+  end
 end
